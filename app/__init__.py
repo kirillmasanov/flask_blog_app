aaaa0094d98e702b093436955a4e040d62e45e39
kirill_masanov <kirill_masanov@mail.ru>
2020-06-08 22:58:59 +0300

    add Flask-Login, protecting routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -7,6 +8,9 @@ from config import config
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -18,6 +22,7 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
