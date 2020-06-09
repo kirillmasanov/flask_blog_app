d384db0438e7e067cd8cb1eb3b73bd4cbbec4978
kirill_masanov <kirill_masanov@mail.ru>
2020-06-09 20:51:42 +0300

    add login form and bootstrap-flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -8,6 +9,7 @@ from config import config
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
@@ -23,6 +25,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    bootstrap.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
