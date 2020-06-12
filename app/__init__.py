bbc720b0d86be3bad09a4599a692516f1e6d2806
kirill_masanov <kirill_masanov@mail.ru>
2020-06-12 22:34:24 +0300

    add user roles

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask_bootstrap import Bootstrap
-from flask import Flask, render_template
+from flask import Flask
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
